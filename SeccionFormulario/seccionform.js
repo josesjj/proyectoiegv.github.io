615e765b1e0f94a90e527d4c4e0a7dba5411d74b
Jose Sanchez <153836552+josesjj@users.noreply.github.com>
2025-09-04 02:38:48 -0400

    arreglando errores y detalles

--- SeccionFormulario/seccionform.js
+++ SeccionFormulario/seccionform.js
@@ -5,15 +5,21 @@ window.location.href = '../Formulario Iniciar Sesion/formlogin.html'
 
 }
 
-const regresar = document.querySelector('#regresar')
 
-regresar.addEventListener('click', ()=> {
+const regresar = document.querySelector('#regresar');
+const regresarMobile = document.querySelector('#regresar-mobile');
 
-alert(`🤗 ¡Hasta pronto! Te esperamos con los brazos abiertos en nuestra institución, donde cada día es una nueva oportunidad para crecer y perfeccionar tu arte culinario. 🍽️✨`)
-localStorage.removeItem('login_succes')
-window.location.href='../Principal/index.html'
+function cerrarSesion() {
+	alert(`🤗 ¡Hasta pronto! Te esperamos con los brazos abiertos en nuestra institución, donde cada día es una nueva oportunidad para crecer y perfeccionar tu arte culinario. 🍽️✨`);
+	localStorage.removeItem('login_succes');
+	window.location.href = '../Formulario Iniciar Sesion/formlogin.html';
+}
 
-
-})
+if (regresar) {
+	regresar.addEventListener('click', cerrarSesion);
+}
+if (regresarMobile) {
+	regresarMobile.addEventListener('click', cerrarSesion);
+}
 
 
